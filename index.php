f88359329da56de5e13a62af575c5b85077c9ca3
xabolsz <abolfazl.c.r.7gaming@gmail.com>
2024-12-22 13:03:47 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+
+<?php 
+  include("header.php")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="site.css">
+    <link rel="stylesheet" href="bootstrap.css">
+
+    
+</head>
+<body>
+
+    <MAIN>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-7 col-xl-7">
+                    
+
+                    
+                </div>
+            </div>
+        </div>
+    
+        <section id="home">
+        <marquee direction="right"><h1>Mr.befroosh</h1></marquee>
+        </section>
+
+
+
+
+        <p><hr></p>
+
+            <DIV id="TABLIGH">
+                <p><h1>فروش</h1></p>
+                <p>(در اصطلاح حقوقی: بیع) به مبادلهٔ یک کالا یا خدمت با پول گفته می‌شود. شخصی که کالا یا خدمت را ارائه می‌دهد فروشنده و شخصی که هزینه را پرداخت می‌کند خریدار نامیده می‌شوند. فروش در بازرگانی فرآیند مبادله ارزش است که در آن فروشنده کالاها و خدمات خود را در ازای دریافت پول یا سایر دارایی ها ارائه می‌کند .عملیات فروش جز اساسی و تکمیل کننده یک فعالیت بازرگانی است[۱].</p>
+
+
+                <video controls autoplay loop muted width="600">
+                    <source src="ll.mp4" type="video/mp4">{}
+                </video>
+                <h2>عوامل موثر در فروش</h2>
+                <ul>
+                    <li>مهارت</li>
+                    <li><audio controls autoplay loop muted><source src="maharat.mp3" type="audio/mpeg"></audio></li>
+                    <p><h6>مهارت فروش یکی از مهمترین عوامل فروش است بعضی ها ان را بر اساس تو جامعه بودن یا ارتباط خوب با مردوم دسته بندی میکنند در صورتی که اینطوی نیست و مهارت فروش چیزی کاملا متفاوت با مهارت اجتماعی است پادکست بالا توضیحه کوتاهی از مهارت فروش است</h6></p>
+                    <li>ظاهر</li>
+                    <li><audio controls autoplay loop muted><source src="zaher.mp3" type="audio/mpeg"></audio></li>
+                    <p><h6>ظاهر یک فروشنده جایه شما صحبت میکند از انجایی که اولین چیزی از فروشنده که خریدار میبیند تیپ و استایل ان است و این باعث میشود که تیپ و استایل چیزه خیلی مهمی باشد به غیر از این م.رد تیپ و استایلیک فروشنده سابقه و میزانه حرفه ان را در فروش و جامعه نشان میدهد پادکست بالا برایه ظاهره یک فروشنده ضبت شده و به شما کمک میکند</h6></p>
+                    <li>زبان بدن</li>
+                    <li><audio controls autoplay loop muted><source src="zaban.mp3" type="audio/mpeg"></audio></li>
+                    <p><h6>زبان بدن میتواند حرفه و سابقه یه شمارو نشان دهد مهم این است که یک فروشنده یک دوره زابن بدن تکمیل کنند چون میتواند در فروش شما اختلال و نساندهنده ناتوانیه شما باش</h6></p>
+                    <hr>
+                </ul>
+            </DIV>
+        
+
+
+    </MAIN>
+
+
+    <link rel="stylesheet" href="bootstrap.js">
+</body>
+</html>
+
+<?php 
+  include("footer.php")
+?>
